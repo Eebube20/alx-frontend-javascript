c8ac17ea5b737252f07de98d66863ee5898fc70a
Elvis Odinigwe <102396151+Elvino03@users.noreply.github.com>
2023-06-29 21:21:59 +0100

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,19 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft);
-    this.floors = floors;
+    super(sqft); // Call the parent class constructor
+
+    this._floors = floors;
   }
 
+  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
-  }
-
+  // Override the evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors.`;
   }
 }
